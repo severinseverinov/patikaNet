35c4b7c1bbd3d6793829aa58f53aa31a7a9f62a9
Severin ISAEV <severinisaev@hotmail.com>
2022-01-02 19:40:21 +0300

    first commit

---
+++ cSharp101/consoleExample1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Lütfen isminizi giriniz : ");
+String name =Console.ReadLine();
+Console.Write("Lütfen soyisminizi giriniz : ");
+String surName =Console.ReadLine();
+Console.WriteLine("Hoşgeldiniz sayın "+name+" "+surName);
