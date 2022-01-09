24a4be88c74989fc97096a3abbbb9e3fdab43973
Severin ISAEV <severinisaev@hotmail.com>
2022-01-09 18:06:36 +0300

    added donusumler and operatorler | patika .NET

---
+++ cSharp101/operatorler/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace operatorler
+{
+    class Proram{
+        static void Main(String[] args){
+            int x=36;
+            x|=2;
+            Console.WriteLine(x);
+
+            if(true^false){
+                Console.WriteLine("true");
+            }else{
+                Console.WriteLine("false");
+            }
+        }
+    }
+}
