1a10f7ff5030ee3661edb0eadc5475caf4e566d0
Severin ISAEV <severinisaev@hotmail.com>
2022-01-18 15:56:15 +0300

    added | for examples with c#

--- cSharp101/forLoopBreak/Program.cs
+++ cSharp101/forLoopBreak/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+for (int i = 1; i <= 10; i++)
+{
+    if (i % 2 == 0)
+        continue;
+    Console.WriteLine(i);
+}
+
+int tekToplam=0;
+int ciftToplam=0;
+
+for(int i=0;i<=2000;i++){
+    if(i%2==1){
+        tekToplam+=i;
+    }else{
+        ciftToplam+=i;
+    }
+}
+Console.WriteLine("0-2000 arası çift sayıların toplamı : "+ciftToplam);
+Console.WriteLine("0-2000 arası tek sayıların toplamı : "+tekToplam);
