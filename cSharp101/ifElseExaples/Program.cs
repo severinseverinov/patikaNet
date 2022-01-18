c3b28afa3cff341785f187a868b4c14d4c2164e3
Severin ISAEV <severinisaev@hotmail.com>
2022-01-18 14:12:30 +0300

    added | C# ifelse example

---
+++ cSharp101/ifElseExaples/Program.cs
@@ -0,0 +1,15 @@
+﻿int not=650;
+if(not<=44){
+    Console.WriteLine("Notun : 1");
+}else if(not>=45 && not<=59){
+    Console.WriteLine("Notun : 2");
+}else if(not>=60 && not<=69){
+    Console.WriteLine("Notun : 3");
+}else if(not>=70 && not<=84){
+    Console.WriteLine("Notun : 2");
+}else if(not>=85 && not<=100){
+    Console.WriteLine("Notun : 2");
+}else{
+    Console.WriteLine("Girilen not geçersiz.(Lütfen 0-100  arası giriniz.)!");
+}
+
