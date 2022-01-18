7e7abe99168ccc3838716442cbcc115d9a040f70
Severin ISAEV <severinisaev@hotmail.com>
2022-01-18 16:28:35 +0300

    added | foreach and while examples c#

---
+++ cSharp101/whileForeach/Program.cs
@@ -0,0 +1,32 @@
+﻿int sayac = 1;
+while (sayac <= 5)
+{
+    Console.WriteLine(sayac);
+    sayac++;
+}
+
+char charakter='a';
+while(charakter<='z'){
+    if(charakter=='z'){
+        Console.Write(charakter);
+    }else{
+    Console.Write(charakter+", ");
+    }
+    charakter++;
+
+}
+Console.WriteLine("");
+
+int[] sayiDizisi = {2,4,5,1,2,4};
+int toplam = 0 ;
+
+foreach(int i in sayiDizisi)
+{
+    toplam += i ;
+}
+Console.WriteLine("Dizi içindeki sayıların toplamı : "+toplam);
+
+String[] arabalar={"BMW","Ford","Toyota","Nissan"};
+foreach(var araba in arabalar){
+    Console.WriteLine(araba);
+}
