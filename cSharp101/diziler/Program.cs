89e3bd7722b8da7a4d9a4d0d21294b2374d01ff8
Severin ISAEV <severinisaev@hotmail.com>
2022-01-19 19:20:52 +0300

    added | Array methods with c#

--- cSharp101/diziler/Program.cs
+++ cSharp101/diziler/Program.cs
@@ -22,3 +22,15 @@ double ortalama = toplam / 20;
 Console.WriteLine("Sayıların toplamı: " + toplam);
 Console.WriteLine("Sayıların ortalaması: " + ortalama);
 
+int[] sayiDizisi = {1,3,4,9,8,7};
+Array.Resize<int>(ref sayiDizisi,12);
+sayiDizisi[6] = 10;
+
+
+Array.Resize<int>(ref sayiDizisi,3);
+
+Console.WriteLine(Array.IndexOf(sayiDizisi,7));
+
+Array.Clear(sayiDizisi,2,2);
+
+Array.Sort(sayiDizisi);
