229448858403a7b165974dace96d78a66d4abc26
Severin ISAEV <severinisaev@hotmail.com>
2022-01-19 18:08:46 +0300

    added | Array example with C#

---
+++ cSharp101/diziler/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] sayiDizisi = new int[20];
+int toplam = 0;
+
+try
+{
+     for (int i = 0; i < sayiDizisi.Length; i++)
+    {
+        Console.Write("Lütfen {0}. sayıyı girin:", i+1);
+        sayiDizisi[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
+
+foreach(var sayi in sayiDizisi){
+    toplam+=sayi;
+}
+double ortalama = toplam / 20;
+Console.WriteLine("Sayıların toplamı: " + toplam);
+Console.WriteLine("Sayıların ortalaması: " + ortalama);
+
