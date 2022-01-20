ca9748fdc99cc09eaf1440927cf92a48c96675c8
Severin ISAEV <severinisaev@hotmail.com>
2022-01-20 10:37:55 +0300

    added | metots examples c#

---
+++ cSharp101/metots/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace metots
+{
+    class Program{
+    public static void Main(string[] args){
+        int a=5;
+        int b=10;
+
+        Console.WriteLine(a+b);
+        int sonuc=Topla(a,b);
+        Console.WriteLine(sonuc);
+
+        Metod ornek =new Metod();
+        ornek.EkranaYazdir(Convert.ToString(sonuc));
+
+        int sonuc2=ornek.ArttirTopla(ref a,ref b);
+        Console.WriteLine(sonuc2);
+    }
+
+    static int Topla(int x, int y){
+        return(x+y);
+    }
+    }
+    class Metod{
+        public void EkranaYazdir(String x){
+            Console.WriteLine(x);
+        }
+
+        public int ArttirTopla(ref int x,ref int y){
+            x++;
+            y++;
+            return x+y;
+        }
+    }
+}
