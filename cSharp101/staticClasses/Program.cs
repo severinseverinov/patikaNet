650fcc4a9fef3c9a7e2ac5efca9670506f64de9d
Severin ISAEV <severinisaev@hotmail.com>
2022-01-25 18:56:13 +0300

    added | enum examples with c#

--- cSharp101/staticClasses/Program.cs
+++ cSharp101/staticClasses/Program.cs
@@ -5,7 +5,22 @@ Calisan emp3=new Calisan("Ali","VELİ","Bakım");
 Console.WriteLine("Çalışan sayısı : {0}",Calisan.CalisanSayisi);
 
 
-Islemler.Topla(25,85);// static olarak tanımlana sınıflara erişim direk olur yeni bir nesne tanımlamaya gerek yok.
+//Islemler.Topla(25,85); static olarak tanımlana sınıflara erişim direk olur yeni bir nesne tanımlamaya gerek yok.
+
+//Enum lar içerisinde değerleri sabit olarak tutmak için kullanılır. ve bellekte sıralı bir şekilde tutulur.
+//Ayrıca enumlar kodun okunurluluğunu arttırır.
+Console.WriteLine(Gunler.Pazar);
+Console.WriteLine((int)Gunler.Cumartesi);
+
+//Programda bazı veriler sabitler halinde tutulacağı zaman sıklıkla başvurulan yapıdır.
+int sicaklik=32;
+if(sicaklik<=(int)HavaDurumu.Normal){
+    Console.WriteLine("Dışarıya çıkmak için biraz daha bekleyelim.");
+}else if(sicaklik>=(int)HavaDurumu.Sıcak){
+    Console.WriteLine("Dışarıya çıkmak içim çok sıcak bir gün.");
+}else if(sicaklik>=(int)HavaDurumu.Normal && sicaklik<=(int)HavaDurumu.CokSıcak){
+    Console.WriteLine("Hadi dışarıya çıkalım.");
+}
 
 
 
@@ -36,4 +51,22 @@ static class Islemler{
     static long Cikar(int sayi1,int sayi2){
         return sayi1-sayi2;
     }
+}
+
+enum Gunler 
+{
+    Pazartesi, 
+    Sali, 
+    Carsamba, 
+    Persembe, 
+    Cuma=23, 
+    Cumartesi, 
+    Pazar
+}
+
+enum HavaDurumu{
+    Soguk=5,
+    Normal=20,
+    Sıcak=25,
+    CokSıcak=30
 }
